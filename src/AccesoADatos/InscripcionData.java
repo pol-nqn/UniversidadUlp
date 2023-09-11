c21274d8bfa895b3280d436404bcc3dceb09a536
NicolasSTuttini <nstuttini@gmail.com>
2023-09-11 12:24:57 -0300

    cambio obtener materia

--- src/AccesoADatos/InscripcionData.java
+++ src/AccesoADatos/InscripcionData.java
@@ -4,10 +4,50 @@
  */
 package AccesoADatos;
 
+import Entidades.*;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author nstut
  */
 public class InscripcionData {
+    private Connection con;
+    private AlumnoData aluData;
+    private MateriaData matData;
+
+    public InscripcionData() {
+        this.con = Conexion.conectar();
+    }
+
+    public List<Materia> obtenerMateriasCursadas(int id){
+        List<Materia> materias = new ArrayList<Materia>();
+        
+        try {
+            String sql = "SELECT inscripcion.idMateria, nombre, anio FROM inscripcion,materia "
+                + "WHERE inscripcion.idMateria = materia.idMateria AND inscripcion.idAlumno = ? ;";
+            
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            
+            Materia materia;
+            while (rs.next()){
+                materia = new Materia();
+                materia.setIdMateria(rs.getInt("idMateria"));
+                materia.setNombre(rs.getString("nombre"));
+                materia.setAnioMateria(rs.getInt("anio"));
+                materias.add(materia);
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(InscripcionData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return materias;
+    }
     
 }
